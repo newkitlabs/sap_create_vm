488dce4db1595ffcca690e5fe2271471380c086d
Nils Koenig <nkoenig@redhat.com>
2023-11-02 15:12:18 +0100

    added lint files, removed unnecessary files

--- playbooks/create.sh
+++
@@ -1,8 +0,0 @@
-set -x
-
-# ansible-galaxy collection install kubernetes.core
-# yum install python3-kubernetes
-
-rsync -av --delete -e 'ssh -o "UserKnownHostsFile /dev/null" -o stricthostkeychecking=no'  $HOME/data/git/cnv-dude dci@provisionhost:
-ssh -t -o "UserKnownHostsFile /dev/null" -o stricthostkeychecking=no dci@provisionhost "sudo ln -sf /home/dci/cnv-dude/roles/ocp_create_sap_hana_vm /usr/share/ansible/roles/"
-ssh -t -o "UserKnownHostsFile /dev/null" -o stricthostkeychecking=no dci@provisionhost "cd cnv-dude/playbooks && ansible-playbook -vv -i localhost, create_sap_hana_vm/create-sap-hana-vm.yml $@"
--- playbooks/lu0560.sh
+++
@@ -1,1 +0,0 @@
-bash create.sh -e dude_machine_name=lu0560 -e dude_vm_id=5 -e vm_template=vm-configuration.yaml.j2 -e dude_vm_memory=5900 -e vm_namespace=openshift-sriov-network-operator
